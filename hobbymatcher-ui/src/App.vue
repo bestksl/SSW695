fd7dc97e749ef51f13c4bb45e397d5c090285249
RobertZSun <robertsunzhe@hotmail.com>
2020-02-12 16:50:04 -0500

    created header for main page

--- hobbymatcher-ui/src/App.vue
+++ hobbymatcher-ui/src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    <img src="./assets/logo-200x200.png">
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
