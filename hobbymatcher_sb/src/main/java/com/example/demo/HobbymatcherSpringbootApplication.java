7c9ea19c45fe280668c2d331bf6735444bc3e82e
bestksl <kslup7@gmail.com>
2020-02-12 18:48:51 -0500

    init

---
+++ hobbymatcher_sb/src/main/java/com/example/demo/HobbymatcherSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HobbymatcherSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HobbymatcherSpringbootApplication.class, args);
+    }
+
+}
