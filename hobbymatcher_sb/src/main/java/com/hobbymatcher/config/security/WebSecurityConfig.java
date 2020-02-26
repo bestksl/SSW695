83636c21623a615716a40c7a0e42bd9c095cba1c
bestksl <kslup7@gmail.com>
2020-02-26 13:59:17 -0500

    fix add user bug

--- hobbymatcher_sb/src/main/java/com/hobbymatcher/config/security/WebSecurityConfig.java
+++ hobbymatcher_sb/src/main/java/com/hobbymatcher/config/security/WebSecurityConfig.java
@@ -55,6 +55,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/webjars/**").permitAll()
                 .antMatchers("/v2/**").permitAll()
                 .antMatchers("/swagger-resources/**").permitAll()
+                .antMatchers(HttpMethod.POST,"/user/").permitAll()
                 .antMatchers("/**")
                 .fullyAuthenticated()
                 .anyRequest()
