9654b25f7e140829b0873c6154da2d59b9116f94
bestksl <kslup7@gmail.com>
2020-04-08 13:54:34 -0400

    first

--- hobbymatcher_sb/src/main/java/com/hobbymatcher/controller/event/EventController.java
+++ hobbymatcher_sb/src/main/java/com/hobbymatcher/controller/event/EventController.java
@@ -49,6 +49,26 @@ public class EventController {
         return resp;
     }
 
+    // list top 5 event
+    @GetMapping("/listRecentEvents")
+    public Map<String, Object> listTop5Events(String hobbyId, HttpServletResponse response) {
+        Map<String, Object> resp = new HashMap<String, Object>();
+        try {
+            if (hobbyId != null && !hobbyId.isEmpty()) {
+                resp.put("list", eventService.listRecentEvents());
+            } else {
+                resp.put("list", eventService.listEvent());
+            }
+            resp.put("success", true);
+            response.setStatus(200);
+        } catch (Exception exp) {
+            exp.printStackTrace();
+            resp.put("status", false);
+            response.setStatus(400);
+        }
+        return resp;
+    }
+
     // add event
     @PostMapping("/addevent")
     public Map<String, Object> addEvent(HttpServletRequest request, HttpServletResponse response, //
@@ -89,89 +109,89 @@ public class EventController {
         return resp;
     }
 
-	@PostMapping("/editevent")
-	public Map<String, Object> editEvent(HttpServletRequest request, HttpServletResponse response, //
-			@ModelAttribute Event event, @RequestPart(name = "file", required = false) MultipartFile imageFile) {
-		Map<String, Object> resp = new HashMap<String, Object>();
-		try {
-			if (imageFile != null && !imageFile.isEmpty()) {
-				event.setPhotoId(FileUtil.transferFile(imageFile));
-			}
-			Boolean updated = eventService.updateEvent(event);
-			resp.put("success", updated);
-			response.setStatus(updated ? 200 : 400);
-		} catch (Exception exp) {
-			exp.printStackTrace();
-			resp.put("success", false);
-			response.setStatus(400);
-		}
-		return resp;
-	}
+    @PostMapping("/editevent")
+    public Map<String, Object> editEvent(HttpServletRequest request, HttpServletResponse response, //
+                                         @ModelAttribute Event event, @RequestPart(name = "file", required = false) MultipartFile imageFile) {
+        Map<String, Object> resp = new HashMap<String, Object>();
+        try {
+            if (imageFile != null && !imageFile.isEmpty()) {
+                event.setPhotoId(FileUtil.transferFile(imageFile));
+            }
+            Boolean updated = eventService.updateEvent(event);
+            resp.put("success", updated);
+            response.setStatus(updated ? 200 : 400);
+        } catch (Exception exp) {
+            exp.printStackTrace();
+            resp.put("success", false);
+            response.setStatus(400);
+        }
+        return resp;
+    }
 
-	@GetMapping("/getparticipation")
-	public Map<String, Object> getParticipation(HttpServletRequest request, HttpServletResponse response, //
-			@RequestParam("id") Integer eventId) {
-		Map<String, Object> resp = new HashMap<String, Object>();
-		try {
-			int userId = authUtilService.getUserId(request);
-			Participation part = eventService.getParticipation(userId, eventId);
-			if (part == null) {
-				resp.put("status", "");
-			} else if (part.getParticipatedOnDatetime() != null) {
-				resp.put("status", "participated");
-			} else if (part.getApprovedOnDatetime() != null) {
-				resp.put("status", "approved");
-			} else if (part.getRequestedOnDatetime() != null) {
-				resp.put("status", "requested");
-			}
-			resp.put("success", true);
-			response.setStatus(200);
-		} catch (Exception exp) {
-			exp.printStackTrace();
-			resp.put("success", false);
-			response.setStatus(400);
-		}
-		return resp;
-	}
+    @GetMapping("/getparticipation")
+    public Map<String, Object> getParticipation(HttpServletRequest request, HttpServletResponse response, //
+                                                @RequestParam("id") Integer eventId) {
+        Map<String, Object> resp = new HashMap<String, Object>();
+        try {
+            int userId = authUtilService.getUserId(request);
+            Participation part = eventService.getParticipation(userId, eventId);
+            if (part == null) {
+                resp.put("status", "");
+            } else if (part.getParticipatedOnDatetime() != null) {
+                resp.put("status", "participated");
+            } else if (part.getApprovedOnDatetime() != null) {
+                resp.put("status", "approved");
+            } else if (part.getRequestedOnDatetime() != null) {
+                resp.put("status", "requested");
+            }
+            resp.put("success", true);
+            response.setStatus(200);
+        } catch (Exception exp) {
+            exp.printStackTrace();
+            resp.put("success", false);
+            response.setStatus(400);
+        }
+        return resp;
+    }
 
-	@PostMapping("/participation")
-	public Map<String, Object> manageParticipation(HttpServletRequest request, HttpServletResponse response, //
-			@RequestParam("id") Integer eventId, @RequestParam("action") String action,
-			@RequestParam(name = "userId", required = false) Integer theUserId) {
-		Map<String, Object> resp = new HashMap<String, Object>();
-		try {
-			int userId = authUtilService.getUserId(request);
-			Participation part = eventService.getParticipation(userId, eventId);
+    @PostMapping("/participation")
+    public Map<String, Object> manageParticipation(HttpServletRequest request, HttpServletResponse response, //
+                                                   @RequestParam("id") Integer eventId, @RequestParam("action") String action,
+                                                   @RequestParam(name = "userId", required = false) Integer theUserId) {
+        Map<String, Object> resp = new HashMap<String, Object>();
+        try {
+            int userId = authUtilService.getUserId(request);
+            Participation part = eventService.getParticipation(userId, eventId);
 
-			if ("request".equals(action)) {
-				if (part == null) {
-					eventService.requestToParticipateEvent(userId, eventId);
-				}
-				resp.put("status", "requested");
-			} else if ("cancel".equals(action)) {
-				if (part != null) {
-					eventService.cancelEventParticipationRequest(userId, eventId);
-				}
-				resp.put("status", "");
-			} else if ("participated".equals(action)) {
-				if (part != null) {
-					eventService.markEventAsParticipated(userId, eventId);
-				}
-				resp.put("status", "participated");
-			} else if ("approve".equals(action)) {
-				if (part != null) {
-					eventService.approveUser(theUserId, eventId);
-				}
-				resp.put("status", "approved");
-			}
+            if ("request".equals(action)) {
+                if (part == null) {
+                    eventService.requestToParticipateEvent(userId, eventId);
+                }
+                resp.put("status", "requested");
+            } else if ("cancel".equals(action)) {
+                if (part != null) {
+                    eventService.cancelEventParticipationRequest(userId, eventId);
+                }
+                resp.put("status", "");
+            } else if ("participated".equals(action)) {
+                if (part != null) {
+                    eventService.markEventAsParticipated(userId, eventId);
+                }
+                resp.put("status", "participated");
+            } else if ("approve".equals(action)) {
+                if (part != null) {
+                    eventService.approveUser(theUserId, eventId);
+                }
+                resp.put("status", "approved");
+            }
 
-			resp.put("success", true);
-			response.setStatus(200);
-		} catch (Exception exp) {
-			exp.printStackTrace();
-			resp.put("success", false);
-			response.setStatus(400);
-		}
-		return resp;
-	}
+            resp.put("success", true);
+            response.setStatus(200);
+        } catch (Exception exp) {
+            exp.printStackTrace();
+            resp.put("success", false);
+            response.setStatus(400);
+        }
+        return resp;
+    }
 }
